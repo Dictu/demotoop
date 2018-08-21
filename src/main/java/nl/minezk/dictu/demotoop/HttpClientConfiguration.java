ad80226ff309e64f6e470de306e1068954b8869f
Sander IJpma <s.ijpma@dictu.nl>
2018-08-21 09:27:03 +0200

    Moved httpclient config to API

--- src/main/java/nl/minezk/dictu/demotoop/HttpClientConfiguration.java
+++ src/main/java/nl/minezk/dictu/demotoop/HttpClientConfiguration.java
@@ -1,94 +1,11 @@
 package nl.minezk.dictu.demotoop;
 
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.net.ssl.SSLContext;
-
-import org.apache.http.HttpHost;
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+
+import eu.toop.node.ProxiedHttpClientConfiguration;
 
 @Configuration
-public class HttpClientConfiguration {
-	
-	@Value("${proxy.host}")
-	public String proxyHost;
-	
-	@Value("${proxy.port}")
-	public int proxyPort;
-	
-	@Bean
-	public HttpHost proxy() {
-		return new HttpHost(proxyHost, proxyPort);
-	}
-	
-	@Bean
-	public List<String> nonProxyHosts() {
-		return  Arrays.asList("localhost", "127.0.0.1");
-	}
-	
-	@Bean
-	public RestTemplate localRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-		HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory();
-		requestFactory.setHttpClient(localHttpClient());
-		RestTemplate restTemplate = new RestTemplate(requestFactory);
-		return restTemplate;
-	}
-	
-	@Bean
-	public RestTemplate restTemplate() 
-	                throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-	    HttpComponentsClientHttpRequestFactory requestFactory =
-	                    new HttpComponentsClientHttpRequestFactory();
-	    requestFactory.setHttpClient(httpClient());
-	    RestTemplate restTemplate = new RestTemplate(requestFactory);
-	    return restTemplate;
-	 }
-	
-	@Bean
-	public TrustStrategy acceptingTrustStrategy() {
-		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-		return acceptingTrustStrategy;
-	}
-	
-	@Bean
-	public HttpClient httpClient() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(acceptingTrustStrategy())
-                .build();
-		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-	    CloseableHttpClient httpClient = HttpClients.custom()
-	    				.setProxy(proxy())
-	                    .setSSLSocketFactory(csf)
-	                    .build();
-	    return httpClient;
-	}
-	
-	@Bean
-	public HttpClient localHttpClient() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(acceptingTrustStrategy())
-                .build();
-		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-	    CloseableHttpClient httpClient = HttpClients.custom()
-	                    .setSSLSocketFactory(csf)
-	                    .build();
-	    return httpClient;
-	}
+public class HttpClientConfiguration extends ProxiedHttpClientConfiguration {
 
 	
 }
